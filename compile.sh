09ee90d55a59ad783bc53e6a8ebf1b938197470b
CABOS Matthieu <38350182+matthieucabos@users.noreply.github.com>
2020-07-22 09:25:17 +0200

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -8,6 +8,7 @@ then
 echo "#####################################################################################"
 echo ""
 echo "Wrong parameters number. Usage : ./compile.sh <mode> <file.c/cpp> <file.c/cpp> ..."
+echo "Use --help option to get help"
 echo ""
 echo "#####################################################################################"
 echo ""
@@ -26,10 +27,43 @@ echo ""
 exit
 fi
 
+function help(){
+	printf "
+#################################################################################
+
+Script usage
+
+This script has been developped to automate the compilation process.
+It treat c, c++ and fortran source files. Compilation can be ruled with two modes :
+	* ) The chain mode realize a chain compilation mode : Each source file is 
+compiled independantly from each other
+	* ) The modular mode realize a modular compilation using one main source file 
+and the dependency modules and functions as source files.
+
+This script take 2 types of arguments : the first one determine the mode between 
+0 (chain) and 1 (modular)
+The others parameters are the source files to compile.
+The source file must be .c, .cpp or .f90 files, others extensions files WILL 
+NOT BE TREATED.
+
+You have to use the correct syntaxe specifying the mode for each exxecution :
+
+./compile.sh <mode> <source file 1> <source file 2> ... <source file n>
+
+#################################################################################
+	"
+}
+
+if [ "$1" = "--help" ]
+	then
+		help
+		exit
+fi
 
 mode=$1
 
 parameters=""
+name=" "
 
 if [ $mode -eq 0 ]                                # Executing script profile in Chain Compilation mode
 	then
@@ -47,11 +81,15 @@ if [ $mode -eq 0 ]                                # Executing script profile in 
 					if [ $e = "c" ]
 						then
 						name=`basename $i '.c'`   # Getting the .exe filename
-						gcc $i -o $name           # Compiling the code file as parameter
+						gcc $i -o $name -fopenmp  # Compiling the code file as parameter
 					elif [ "$e" = "cpp" ]
 						then
 						name=`basename $i '.cpp'` # Getting the .exe filename
-						g++ $i -o $name           # Compiling the code file as parameter
+						g++ $i -o $name -fopenmp  # Compiling the code file as parameter
+					elif [ "$e" = "f90" ]
+						then
+						name=`basename $i '.f90'` # Getting the .exe filename
+						gfortran -o $name $i -fopenmp
 					fi
 				done
 elif [ $mode -eq 1 ]                              # Executing script profile in Modular Compilation mode
@@ -77,10 +115,16 @@ elif [ $mode -eq 1 ]                              # Executing script profile in 
 
 				if [ $e = "c" ]
 					then
-						gcc $parameters -o $name  # Compiling the Modular file as parameters
+						gcc $parameters -o $name -fopenmp  # Compiling the Modular file as parameters
 				elif [ $e = "cpp" ]
 					then
-						g++ $parameters -o $name  # Compiling the Modular file as parameters
+						g++ $parameters -o $name -fopenmp  # Compiling the Modular file as parameters
+				elif [ $e = "f90" ]
+					then
+						for i in $parameters
+							do
+								name=`basename $i '.f90'`
+								gfortran -o $name $i -fopenmp
+							done
 				fi
 fi
-
