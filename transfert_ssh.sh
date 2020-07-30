d64eacd33e6c1fc744701b5bcf142651257a4d05
CABOS Matthieu <38350182+matthieucabos@users.noreply.github.com>
2020-07-30 14:06:20 +0200

    Add files via upload

--- transfert_ssh.sh
+++ transfert_ssh.sh
@@ -5,7 +5,7 @@
 function usage(){
 	printf"
 Please to use the script with the correct number of arguments :
-./transfert.sh <mode> <filename> <user> <source folder> <destination folder> <ip>
+./transfert.sh <mode> <user> <source folder> <destination folder> <ip> <filename>
 
 Where :
 * mode is the way to transfert between
@@ -13,18 +13,34 @@ Where :
 	1 mean download file since the ssh root directory
 * filename is the exact file name to transfert
 * user is your standard user name on the ssh plateform
-* local folder is the source repertory
-* destination folder is the destination repertory
-* ip is the ssh adress to connect
 	"
 }
 
-mode=$1
-file=$2
-user=$3
-source=$4
-dest=$5
-IP=$6
+param=$*
+ind=1
+files=""
+for i in $param
+	do
+		if [ $ind -eq 1 ]
+		then
+			mode=$i
+		elif [ $ind -eq 2 ]
+			then
+				user=$i 
+		elif [ $ind -eq 3 ]
+			then
+				source=$i
+		elif [ $ind -eq 4 ]
+			then
+				dest=$i 
+		elif [ $ind -eq 5 ]
+			then
+				ip=$i
+		else
+			files="$files"" $i"
+		fi
+		ind=$((ind+1))
+	done
 index=0
 
 
@@ -58,7 +74,7 @@ for i in $dir
 		fi
 	done
 
-dir=`ssh $user@$IP 'find'`      # Getting the Folders Architecture
+dir=`ssh $user@$ip 'find'`      # Getting the Folders Architecture
 
 for i in $dir
 	do
@@ -69,14 +85,25 @@ for i in $dir
 		fi
 	done
 
+for i in $files
+	do
+		if [ $mode -eq 0 ]
+			then
+				file="$file"" ""$source_way""/""$i"
+		elif [ $mode -eq 1 ]
+			then
+				file="$file"" ""$dest_way""/""$i"
+		fi
+	done
 
 if [ $mode -eq 0 ]
 	then
-		file="$source_way""/""$file"
-		echo $file
-		scp $file $user@$IP:$dest_way
+		# file="$source_way""/""$i"
+		scp $file $user@$ip:$dest_way 
 elif [ $mode -eq 1 ]
 	then
-		file="$dest_way""/""$file"
-		scp $user@$IP:./$file $source_way
+		for i in $file
+			do
+				scp $user@$ip:./$i $source_way
+			done
 fi
