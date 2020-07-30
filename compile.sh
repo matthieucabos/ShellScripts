ea62eb0cf596ffb056d1f5de0cb3d241e2c8bb87
CABOS Matthieu <38350182+matthieucabos@users.noreply.github.com>
2020-07-30 10:12:17 +0200

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -17,12 +17,23 @@ echo ""
 echo "#####################################################################################"
 echo ""
 echo "mode = 0 => A Chain Compilation Profile using INDEPENDANTS C/C++ files."
-echo "Using the syntaxe ./compile.sh 0 <file1.c/cpp> <file2.c/cpp> ..."
+echo "Using the syntaxe ./compile.sh 0 <file1.c/cpp/f90> <file2.c/cpp/f90> ..."
 echo ""
 echo "#####################################################################################"
 echo ""
 echo "mode = 1 => A Modulare Compilation Profile." 
-echo "Using the syntaxe ./compile.sh 1 <main.c/cpp> <modular_part1.c/cpp> <modular_part2.c/cpp> ..." 
+echo "Using the syntaxe ./compile.sh 1 <main.c/cpp/f90> <modular_part1.c/cpp/f90>"
+echo " <modular_part2.c/cpp/f90> ..." 
+echo ""
+echo "#####################################################################################"
+echo ""
+echo "mode = 2 => A MPI Compilation Profile." 
+echo "Using the syntaxe ./compile.sh 2 <file1.c/cpp/f90> <file2.c/cpp/f90> ..." 
+echo ""
+echo "#####################################################################################"
+echo ""
+echo "mode = 3 => An openmp Compilation Profile." 
+echo "Using the syntaxe ./compile.sh 3 <file1.c/cpp/f90> <file2.c/cpp/f90> ..." 
 echo ""
 exit
 fi
@@ -41,7 +52,7 @@ compiled independantly from each other
 and the dependency modules and functions as source files.
 
 This script take 2 types of arguments : the first one determine the mode between 
-0 (chain) and 1 (modular)
+0 (chain),  1 (modular), 2 (mpi compilation), 3 (openmp compilation)
 The others parameters are the source files to compile.
 The source file must be .c, .cpp or .f90 files, others extensions files WILL 
 NOT BE TREATED.
@@ -64,6 +75,7 @@ mode=$1
 
 parameters=""
 name=" "
+mpiFlag=0
 
 if [ $mode -eq 0 ]                                # Executing script profile in Chain Compilation mode
 	then
@@ -81,15 +93,30 @@ if [ $mode -eq 0 ]                                # Executing script profile in 
 					if [ $e = "c" ]
 						then
 						name=`basename $i '.c'`   # Getting the .exe filename
-						gcc $i -o $name -fopenmp  # Compiling the code file as parameter
+						gcc $i -o $name           # Compiling the code file as parameter
 					elif [ "$e" = "cpp" ]
 						then
 						name=`basename $i '.cpp'` # Getting the .exe filename
-						g++ $i -o $name -fopenmp  # Compiling the code file as parameter
-					elif [ "$e" = "f90" ]
+						g++ $i -o $name           # Compiling the code file as parameter
+					elif [ "$e" = "f90" -o "$e" = "f77" -o "$e" = "f95" -o "$e" = "FOR" ]
 						then
-						name=`basename $i '.f90'` # Getting the .exe filename
-						gfortran -o $name $i -fopenmp
+							e=".""$e"
+							name=`basename $i $e` # Getting the .exe filename
+							if [ "$e" = ".f77" -o "$e" = ".FOR"  ]
+								then	
+									if [ "$e" = ".f77" ]
+										then		
+											e=`basename $i '.f77'`	
+									elif [ "$e" = ".FOR" ]
+										then 
+											e=`basename $i '.FOR'`	
+									fi
+									e="$e"".f"		
+									mv $i $e									
+									gfortran $e -o $name
+							else
+									gfortran -o $name $i
+							fi
 					fi
 				done
 elif [ $mode -eq 1 ]                              # Executing script profile in Modular Compilation mode
@@ -101,15 +128,110 @@ elif [ $mode -eq 1 ]                              # Executing script profile in 
 						parameters=$parameters" "$i
 				fi
 			done
+		for i in $parameters
+			do
+				e=${i#*.}                     # Getting the file extension
+				if [ $e = "c" ]
+					then
+					name=`basename $i '.c'`   # Getting the .exe filename
+					break
+				elif [ "$e" = "cpp" ]
+					then
+					name=`basename $i '.cpp'` # Getting the .exe filename
+					break
+				fi
+			done
+		if [ $e = "c" ]
+			then
+				gcc $parameters -o $name   # Compiling the Modular file as parameters
+		elif [ $e = "cpp" ]
+			then
+				g++ $parameters -o $name   # Compiling the Modular file as parameters
+		elif [ "$e" = "f90" -o "$e" = "f77" -o "$e" = "f95" -o "$e" = "FOR" ]
+			then
+				for i in $parameters
+					do
+					e=${i#*.}
+					e=".""$e"
+					name=`basename $i $e`
+					if [ "$e" = ".f77" -o "$e" = ".FOR"  ]
+						then	
+							if [ "$e" = ".f77" ]
+								then		
+									e=`basename $i '.f77'`	
+							elif [ "$e" = ".FOR" ]
+								then 
+									e=`basename $i '.FOR'`	
+							fi
+							e="$e"".f"		
+							mv $i $e									
+							gfortran $e -o $name
+					else
+							gfortran -o $name $i
+					fi
+				done
+		fi
+elif [ $mode -eq 2 ]
+	then
+		for i in $@
+			do
+				if [ "$i" != "2" ]
+					then
+						parameters=$parameters" "$i
+				fi
+			done
 			for i in $parameters
 				do
 					e=${i#*.}                     # Getting the file extension
 					if [ $e = "c" ]
 						then
 						name=`basename $i '.c'`   # Getting the .exe filename
+						mpicc -o $name $i         # Compiling the code file as parameter
 					elif [ "$e" = "cpp" ]
 						then
 						name=`basename $i '.cpp'` # Getting the .exe filename
+						mpicxx -o $name $i        # Compiling the code file as parameter
+					elif [ "$e" = "f90" -o "$e" = "f77" -o "$e" = "f95" -o "$e" = "FOR" ]
+						then
+						e=".""$e"
+						name=`basename $i $e` # Getting the .exe filename
+						if [ "$e" = ".f77" -o "$e" = ".FOR"  ]
+							then	
+								if [ "$e" = ".f77" ]
+									then		
+										e=`basename $i '.f77'`	
+								elif [ "$e" = ".FOR" ]
+									then 
+										e=`basename $i '.FOR'`	
+								fi
+								e="$e"".f"	
+								mv $i $e
+								mpifort -o $name $e
+						else
+								mpifort -o $name $i
+						fi
+					fi
+				done
+elif [ $mode -eq 3 ]
+	then
+		for i in $@
+			do
+				if [ "$i" != "3" ]                # Rebuilding the file name parameters list
+					then
+						parameters=$parameters" "$i
+				fi
+			done
+			for i in $parameters
+				do
+					e=${i#*.}                     # Getting the file extension
+					if [ $e = "c" ]
+						then
+						name=`basename $i '.c'`   # Getting the .exe filename
+						break
+					elif [ "$e" = "cpp" ]
+						then
+						name=`basename $i '.cpp'` # Getting the .exe filename
+						break
 					fi
 				done
 
@@ -119,12 +241,28 @@ elif [ $mode -eq 1 ]                              # Executing script profile in 
 				elif [ $e = "cpp" ]
 					then
 						g++ $parameters -o $name -fopenmp  # Compiling the Modular file as parameters
-				elif [ $e = "f90" ]
+				elif [ $e = "f90" -o $e = "f77" -o $e = "f95" -o "$e" = "FOR" ]
 					then
 						for i in $parameters
 							do
-								name=`basename $i '.f90'`
-								gfortran -o $name $i -fopenmp
+								e=${i#*.}
+								e=".""$e"
+								name=`basename $i $e`
+								if [ "$e" = ".f77" -o "$e" = ".FOR"  ]
+									then	
+										if [ "$e" = ".f77" ]
+											then		
+												e=`basename $i '.f77'`	
+										elif [ "$e" = ".FOR" ]
+											then 
+												e=`basename $i '.FOR'`	
+										fi
+										e="$e"".f"		
+										mv $i $e			
+										gfortran $e -o $name -fopenmp
+								else
+										gfortran -o $name $i -fopenmp
+								fi
 							done
 				fi
 fi
