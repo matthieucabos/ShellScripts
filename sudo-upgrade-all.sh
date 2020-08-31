648eb4038d064d06934fb845c3e8abaa922abd88
CABOS Matthieu <38350182+matthieucabos@users.noreply.github.com>
2020-08-31 13:21:24 +0200

    Upgrade all librairies contained on a Unix OS.
    Upgrade all librairies contained on a Unix OS.

---
+++ sudo-upgrade-all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Author : CABOS Matthieu
+# Date : 31/08/2020
+
+rez=`apt list --upgradable`
+for i in $rez
+do
+	libname=`echo $i | grep "/" `
+	if [ ! "$libname" = "" ]
+	then
+		lib=`echo $i | cut -d "/" -f1`
+		lib_final="$lib_final"" ""$lib"
+		libname=""
+	fi 
+done
+for i in $lib_final
+do
+apt upgrade $i
+done
